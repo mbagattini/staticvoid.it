caea07e8397b7d097a92779873c394d185488128
Matteo Bagattini <m.bagattini@gmail.com>
2025-03-30 19:08:40 +0200

    Added more commands and info

--- StaticVoid/app.js
+++ StaticVoid/app.js
@@ -5,6 +5,11 @@ document.addEventListener('DOMContentLoaded', async () => {
     const commandLineElement = document.getElementById('prompt');
     const promptElement = document.getElementById('prompt-label');
     const bannerElement = document.getElementById('banner');
+    
+    //sets the current language according to the user's browser
+    //TODO: implement localization
+    let currentLanguage = getBrowserLanguage();
+    
     //textual content of the prompt-label
     const promptText = "guest@staticvoid.it ~ #";
     
@@ -40,6 +45,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             addCommandToHistory(command);
             await executeCommand(command);
             commandLineElement.value = '';
+            scrollToBottom();
         } else if (event.key === 'ArrowUp') {
             event.preventDefault();
             if (historyIndex < history.length - 1) {
@@ -57,32 +63,46 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     //executes a command received via text input
     async function executeCommand(userInput) {
-        appendToOutput(`${promptText} ${userInput}`);
-        const args = userInput.split(' ');
-        const command = args[0].toLowerCase();
+        appendToOutput(`${promptText} ${userInput}<br/>`);
+        const args = userInput.split(' ').slice(1);
+        const command = userInput.split(' ')[0].toLowerCase();
+        
+        // if(isDebug) {
+        //     appendToOutput("<div class='response'>");
+        //     appendToOutput(`Command: ${command}<br>`);
+        //     appendToOutput(`Args: ${args}<br>`);
+        //     appendToOutput("</div>");
+        // }
+        
         switch (command) {
-            case 'clear':
-                clearOutput();
+            case "clear":
+                executeClearCommand();
                 break;
-            case 'help':
-            case 'qrcode':
-            case 'who':
+            case "help":
+            case "hello":
+            case "qrcode":
+            case "contacts":
+            case "cookies":
+            case "privacy":
+            case "ver":
+            case "who":
                 await executeHtmlOutputCommand(command);
                 break;
-            case 'echo':
-                if (args.length > 1) {
-                    const message = args.slice(1).join(' ');
-                    appendToOutput(message);
-                } else {
-                    appendToOutput('Missing argument for echo command.');
-                }
+            case "linkedin":
+                executeNavigateToCommand("https://www.linkedin.com/in/matteobagattini/");
                 break;
-            case '':
-                //empty command, ignore
-                appendToOutput("<br/>");
+            case "cv":
+                executeNavigateToCommand("assets/matteo-bagattini-cv.pdf");
+                break;
+            case "github":
+                executeNavigateToCommand("https://github.com/mbagattini");
+                break;
+            case "":
+                //empty command
+                appendToOutput("");
                 break;
             default:
-                appendToOutput(`> Command not found: ${command}`);
+                await executeHtmlOutputCommand("notfound");
         }
     }
     
@@ -94,21 +114,39 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
     }
 
-    //appends new content to the history of the terminal
+    //appends new content to the terminal's buffer
     function appendToOutput(html) {
         outputElement.innerHTML += html;
     }
     
     //clears the output
-    function clearOutput() {
+    function executeClearCommand() {
         initUI();
     }
+    
+    //change the UI language
+    // function executeLangCommand(args) {
+    //     //check if language is specified
+    //     if(args.length === 0) {
+    //         appendToOutput("<div class='response'>");
+    //         appendToOutput("> Invalid or missing argument: language_id<br/>");
+    //         appendToOutput("  Usage: lang --language_id><br/>");
+    //         appendToOutput("    --en: sets current language to English<br/>");
+    //         appendToOutput("    --it: sets current language to Italian");
+    //     }
+    // }
     
     //some commands simply display textual content upon invocation; the output for the command
     //is stored in a text file having the command as file name
     async function executeHtmlOutputCommand(command) {
         const output = await fetchExternalTextContent(command);
         appendToOutput("<div class='response'>" + output + "</div>");
+    }
+    
+    //runs a command that opens a new browser window heading to the specified url
+    function executeNavigateToCommand(url) {
+        appendToOutput(`<div class='response'>> Navigating to ${url}...</div>`);
+        window.open(url, "_blank");
     }
     
     //loads content from an external file
@@ -121,6 +159,15 @@ document.addEventListener('DOMContentLoaded', async () => {
         } catch (error) {
             return 'Error loading file: ' + error;
         }
+    }
+
+    //returns "it" if the current browser is in Italian, "en" otherwise
+    function getBrowserLanguage() {
+        return navigator.language;
+    }
+    
+    function scrollToBottom() {
+        commandLineElement.scrollIntoView({ behavior: 'smooth', block: 'end' });
     }
     
 });
